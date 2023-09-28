a01d0902ec2ee62b775b5b1f883484dbb263d00f
Ashvini2121 <143926882+Ashvini2121@users.noreply.github.com>
2023-09-28 22:15:22 +0530

    1st Task

--- NumberGuessGame.java
+++ NumberGuessGame.java
@@ -1,42 +1,60 @@
-package CODSOFT;
-
 import java.util.Scanner;
 import java.util.Random;
 public class NumberGuessGame{
+    
     public static void main(String[] args) {
+
         Random rand=new Random();
+
         int numberToGuess=rand.nextInt(100)+1;
+
         Scanner sc=new Scanner(System.in);
-        int guess;
-         numberToGuess ++ ;
+
+        int attempts=0;
+
+        int maxAttempts=10;
     
         System.out.println("Welcome the Number Guess Game!");
+
         System.out.println("Guess a number between 1 to 100 : ");
         
        for(int i=1;i<=10;i++){
-        for(int j=1;j<=10;j++){
-            guess=sc.nextInt();
-            if(guess==numberToGuess){
-                System.out.println("Congratulation , You guess the Correct Number !");
-                break;
         
+        for(int j=1;j<=10;j++){
+          
+
+           while (attempts < maxAttempts) {
+
+                System.out.print("Enter your guess: ");
+
+               int guess = sc.nextInt();
+
+              attempts++;
+
+              maxAttempts++;
+
+            if(guess==numberToGuess){
+
+                System.out.println("Congratulation , You guess the Correct Number !"+ attempts + " attempts.");
+
+                break;
             }
             else if(guess<numberToGuess){
+
                 System.out.println(" Your guess is Too Low .  Try again : ");
             }
             else{
                 System.out.println("Your guess is Too High . Try again : ");
-    
             }
-
         }
             System.out.println("Play Again: ");
-        
-    
-    
         }
-        
-    }
-
+     }    
+          sc.close();
+  }
+  
 }
+
+
+
  
